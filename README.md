8d6ed4f0b4954d932ec2868929af57ad674cb8a2
saamukai <contato.samukajpc@gmail.com>
2023-02-14 14:52:55 -0300

    todolist api finish

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # todolist-API
-API de uma lista de tarefas criada em python utilizando fastAPI.
+API de uma lista de tarefas criada em python utilizando fastAPI. Realizada afins de aprendizagem sobre API's. 
+### Caracteristicas da API
+- Criar nova tarefa
+- Listar terafas por:
+    - realizadas
+    - não realizadas
+    - todas as tarefas
+- Atualizar tarefa para:
+    - realizada
+    - não realizada
+- Deletar tarefa
---
+++ todolist-API.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from datetime import date
+
+app = FastAPI()
+
+class Task(BaseModel):
+    tarefa: str
+    realizada: bool
+    prazo: Optional[date]
+
+task_list = [
+    Task(tarefa='teste', realizada=False),
+]
+
+@app.post('/newtask')
+def newTask(todo_task: Task):
+    try:
+        task_list.append(todo_task)
+        return {'status': 'task adicionada'}
+    except:
+        return {'status': 'erro'}
+
+@app.post('/listtask')
+def listTaskFilter(opcao: int = 0):
+    if opcao == 0:
+        return task_list
+    elif opcao == 1:
+        return list(filter(lambda x: x.realizada == False, task_list))
+    elif opcao == 2:
+        return list(filter(lambda x: x.realizada == True, task_list))
+
+
+@app.get('/task/{id}')
+def getTaskByID(id: int):
+    try:
+        return task_list[id]
+    except:
+        return {'status':'erro'}
+
+@app.get('/alltasks')
+def listAllTasks():
+        return task_list
+
+
+@app.post('/updateTask')
+def updateTask(id: int):
+    try:
+        task_list[id].realizada = not task_list[id].realizada
+    except:
+        return {'status':'erro'}
+
+@app.delete('/deletetask')
+def deleteTask(id: int):
+    try:
+        del task_list[id]
+        return 'task deletada '
+    except:
+        return {'status':'erro'}
